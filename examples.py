044cb3b875a9bcf488353f9b6cd07d9873634ac5
Vincent Ren <vincent.ren@indexexchange.com>
2017-12-10 23:18:16 -0500

    Update examples

--- examples.py
+++ examples.py
@@ -1,14 +1,40 @@
 # TODO
 # I need prepared examples of common tasks
 
-class TreeNode
+a = range(10)
+b = range(100,110)
+c = ['apple', 'banana', 'cranberry', 'contoloupe', 'cantaloupe', 'durian', 'fig']
 
-dfs
-bfs
+print('a', list(a))
+print('b', list(b))
+print()
 
-Graphs
-dfs
-bfs
-A*
+tmp = zip(a,b)
+print('zip', list(tmp))
+
+tmp = map(lambda x: x+5, a)
+print('map', list(tmp))
+
+tmp = sorted(c)
+print('lexico sort', tmp)
+
+tmp = sorted(c, key=len)
+print('len sort', tmp)
+
+tmp = sorted(c, key=len, reverse=True)
+print('len sort', tmp)
 
 
+
+# class TreeNode
+
+# dfs
+# bfs
+
+# Graphs
+# dfs
+# bfs
+# A*
+
+
+print()
